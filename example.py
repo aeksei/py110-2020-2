6e39b2fb331822c640fb505fcc1c888b311147b4
aeksei <aeksei94@gmail.com>
2020-05-07 20:10:34 +0300

    add example

---
+++ example.py
@@ -0,0 +1,13 @@
+import re
+import operator
+
+FILENAME = 'example.txt'
+oper = {"+": operator.add}
+
+example = re.compile()  # скобочные группы
+
+with
+    for line in f:
+        result = example.  # парсинг строки
+        oper[result[1]](result[0], result[2]) == result[3]
+
