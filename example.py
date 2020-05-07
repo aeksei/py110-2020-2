3a76cee2529266b107bc125b4e0e9a7a5d8adb8c
aeksei <aeksei94@gmail.com>
2020-05-07 20:53:06 +0300

    add regex

--- example.py
+++ example.py
@@ -4,10 +4,17 @@ import operator
 FILENAME = 'example.txt'
 oper = {"+": operator.add}
 
-example = re.compile()  # скобочные группы
+example = re.compile(r"""
+    (?P<first>\d+\.?\d*)(?:\ *)  # первое число
+    (?P<operator>[+-/*])(?:\ *)  # арифметический оператор
+    (?P<second>\d+\.?\d*)(?:\ *)=(?:\ *)  # второе число
+    (?P<result>\d+\.?\d*)  # результат
+    """, re.VERBOSE)  # скобочные группы
 
-with
+with open(FILENAME, 'r') as f:
     for line in f:
-        result = example.  # парсинг строки
-        oper[result[1]](result[0], result[2]) == result[3]
+        result = example.fullmatch(line.rstrip())  # парсинг строки
+        print(result)
+        if result is not None:
+            print(oper[result['operator']](float(result['first']), float(result['second'])) == float(result['result']))
 
